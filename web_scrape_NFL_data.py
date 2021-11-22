a98cef483ed40d3ebaa11ecce435308320b6fd9b
landon <landonhill07@gmail.com>
2021-11-22 11:46:00 -0500

    web-scrapped rankings from teamrankings.com, need to do this for each week/year now

--- web_scrape_NFL_data.py
+++ web_scrape_NFL_data.py
@@ -1,16 +1,34 @@
 import time
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 
+def main():
+    chrome_options = Options()
+    chrome_options.headless = True # also works
+    driver = webdriver.Chrome('/Users/landon/Downloads/chromedriver', options=chrome_options)  # Optional argument, if not specified will search path.
 
-driver = webdriver.Chrome('/Users/landon/Downloads/chromedriver')  # Optional argument, if not specified will search path.
+    driver.get('https://www.teamrankings.com/nfl/rankings/teams/')
+    get_ranks(driver)
 
-driver.get('https://www.teamrankings.com/nfl/rankings/teams/')
+    driver.quit()
 
-search_box = driver.find_element(By.XPATH, "//table[@id='DataTables_Table_0']")
-print(search_box)
 
-time.sleep(3)
-driver.quit()
+def get_ranks(driver):
+    search_box = driver.find_element(By.XPATH, "//table[@id='DataTables_Table_0']")
+    table_body = search_box.find_element(By.XPATH, ".//tbody")
+    table_rows = table_body.find_elements(By.XPATH, ".//tr")
+
+    for row in table_rows:
+        row_data = []
+        for col in row.find_elements(By.XPATH, ".//td"):
+            row_data.append(col.text)
+
+        team, predictive_rank, home_rank, away_rank, last_5_rank, in_div_rank, strength_of_schedule = row_data
+        print(f'{team}: {predictive_rank}')
+
+
+if __name__ == '__main__':
+    main()
