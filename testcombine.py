58fa8876c9511d2b228ac69e73c2236a688ee823
Parth Patel <maganp00gmail.com>
2021-11-30 12:19:21 -0500

    constructed final dataset

--- testcombine.py
+++ testcombine.py
@@ -6,28 +6,24 @@ import time
 
 
 rank_df = pd.read_csv('expected.csv')
-print(rank_df)
+# print(rank_df)
 
 master = pd.read_csv('master_schedule.csv')
-print(master)
+# print(master)
 
-<<<<<<< HEAD
-print(merged)
-
-=======
-print(rank_df.dtypes)
-print(master.dtypes)
+# print(rank_df.dtypes)
+# print(master.dtypes)
 
 merged = pd.merge(rank_df,master,how="left",on=["team","year","month","day"])
 
-print(merged)
+# print(merged)
 
 margin = pd.read_csv('scoring_margin.csv')
-print(margin.dtypes)
+# print(margin.dtypes)
 ##margin['year']= pd.to_numeric(margin['year'],errors='coerce').isnull()
 ##print(margin[pd.to_numeric(margin['year'],errors='coerce').isnull()])
 
-print(margin.dtypes)
+# print(margin.dtypes)
 
 full = pd.merge(merged,margin,how="left",on=["team","year","month","day"])
 
@@ -35,4 +31,3 @@ full = pd.merge(merged,margin,how="left",on=["team","year","month","day"])
 print(full)
 
 full.to_csv('test.csv')
->>>>>>> 32005faa2e874970962c3afcc3eb66ac642dd580
